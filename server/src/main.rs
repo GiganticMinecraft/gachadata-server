0e765149c83f6e1ec5206746002ff09b43071791
rito528 <39003544+rito528@users.noreply.github.com>
2023-07-23 00:11:58 +0900

    chore: Configのstructを定義

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,24 @@
+mod config {
+
+    pub struct HttpPort {
+        pub port: u16
+    }
+
+    pub struct MySQL {
+        pub address: String,
+        pub port: u16,
+        pub user: String,
+        pub password: String
+    }
+
+    pub struct Config {
+        pub http_port: HttpPort,
+        pub mysql: MySQL
+    }
+
+}
+
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
